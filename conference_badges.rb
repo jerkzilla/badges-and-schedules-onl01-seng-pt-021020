78ada368b58c8fba675b4130f5319fc839fbacf3
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-16 03:38:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end
 
-  def batch_badge_creator (attendees)
- attendees.collect do |name| badge_maker (name)
+  def batch_badge_creator (people)
+ people.collect do |name| badge_maker (name)
  end
  end
